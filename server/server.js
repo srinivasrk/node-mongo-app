69a56fd5844e102cf59bb0998028c3363ff07552
srinivasrk <srinivasrk92@gmail.com>
2018-03-17 16:44:22 -0400

    added GET /todo/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {User} = require('./models/user.js');
@@ -27,6 +28,27 @@ app.get('/todos',(req, res) => {
   }, (err) => {
     res.status(400).send(err);
   });
+});
+
+//GET /todos/_id
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }, (err) => {
+    res.status(400).send();
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
